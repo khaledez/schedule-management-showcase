88185e26e5831b7dcc79f5600b35dccd9c57ebbd
mahmoud- <mahmoudihmaid@gmail.com>
2021-02-09 15:05:45 +0200

    Mahmoud [ADD] custom logger from npm module

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { registerApp } from '@mon-medic/common';
+import { registerApp, CustomLoggerService } from '@mon-medic/common';
 import { ValidationPipe, Logger } from '@nestjs/common';
 import { CONFIG_SERVICE, PORT, SERVICE_NAME } from './common/constants';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new CustomLoggerService(),
+    cors: true,
+  });
 
   const appConfig = app.get(CONFIG_SERVICE);
   const serviceName = appConfig.get(SERVICE_NAME);
