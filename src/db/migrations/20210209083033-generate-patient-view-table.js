96b06cb630d6cc0f22d577e79165b224348a783c
mahmoud- <mahmoudihmaid@gmail.com>
2021-02-09 13:26:36 +0200

    Mahmoud [ADD] fix database name

--- src/db/migrations/20210209083033-generate-patient-view-table.js
+++ src/db/migrations/20210209083033-generate-patient-view-table.js
@@ -11,21 +11,21 @@ module.exports = {
     */
     return queryInterface.sequelize
       .query(
-        'CREATE VIEW `patients_phone_number_view` AS SELECT PATIENT_PHONE_NUMBERS.patient_id, PATIENT_PHONE_NUMBERS.primary,  PHONE_NUMBERS.phone_type_code, PHONE_NUMBERS.number as phone_number FROM patientDB.PatientPhoneNumbers AS PATIENT_PHONE_NUMBERS INNER JOIN patientDB.PhoneNumbers AS PHONE_NUMBERS ON PATIENT_PHONE_NUMBERS.phone_number_id = PHONE_NUMBERS.id WHERE PATIENT_PHONE_NUMBERS.deleted_at IS NULL',
+        'CREATE VIEW `patients_phone_number_view` AS SELECT PATIENT_PHONE_NUMBERS.patient_id, PATIENT_PHONE_NUMBERS.primary,  PHONE_NUMBERS.phone_type_code, PHONE_NUMBERS.number as phone_number FROM PatientManagement.PatientPhoneNumbers AS PATIENT_PHONE_NUMBERS INNER JOIN PatientManagement.PhoneNumbers AS PHONE_NUMBERS ON PATIENT_PHONE_NUMBERS.phone_number_id = PHONE_NUMBERS.id WHERE PATIENT_PHONE_NUMBERS.deleted_at IS NULL',
       )
       .then(() => {
         queryInterface.sequelize.query(
-          "CREATE VIEW `patient_info_view` AS SELECT PATIENT.id AS patient_id, CONCAT(PATIENT.first_name, ' ', PATIENT.last_name) AS full_name,  PATIENT.first_name, PATIENT.last_name, PATIENT.clinic_id, PATIENT.dob, PATIENT.created_at, PATIENT.created_by, PATIENT.updated_at, PATIENT.updated_by, PATIENT.deleted_at, PATIENT.deleted_by, PATIENT_P_H_P.number as primary_health_plan_number FROM patientDB.Patients as PATIENT INNER JOIN patientDB.PatientPrimaryHealthPlans AS PATIENT_P_H_P ON PATIENT.id=PATIENT_P_H_P.patient_id WHERE PATIENT.deleted_at IS NULL",
+          "CREATE VIEW `patient_info_view` AS SELECT PATIENT.id AS patient_id, CONCAT(PATIENT.first_name, ' ', PATIENT.last_name) AS full_name,  PATIENT.first_name, PATIENT.last_name, PATIENT.clinic_id, PATIENT.dob, PATIENT.created_at, PATIENT.created_by, PATIENT.updated_at, PATIENT.updated_by, PATIENT.deleted_at, PATIENT.deleted_by, PATIENT_P_H_P.number as primary_health_plan_number FROM PatientManagement.Patients as PATIENT INNER JOIN PatientManagement.PatientPrimaryHealthPlans AS PATIENT_P_H_P ON PATIENT.id=PATIENT_P_H_P.patient_id WHERE PATIENT.deleted_at IS NULL",
         );
       })
       .then(() => {
         queryInterface.sequelize.query(
-          "CREATE VIEW `patient_view` AS SELECT PATIENT_INFO_VIEW.patient_id AS id, PATIENT_INFO_VIEW.clinic_id, PATIENT_INFO_VIEW.primary_health_plan_number, CONCAT(PATIENT_INFO_VIEW.first_name, ' ', PATIENT_INFO_VIEW.last_name) AS full_name, PATIENT_INFO_VIEW.dob, PATIENT_INFO_VIEW.created_at, PATIENT_INFO_VIEW.created_by, PATIENT_INFO_VIEW.updated_at, PATIENT_INFO_VIEW.updated_by, PATIENT_INFO_VIEW.deleted_at, PATIENT_INFO_VIEW.deleted_by, PATIENT_PHONE_VIEW_NUMBERS.primary, PATIENT_PHONE_VIEW_NUMBERS.phone_type_code, PATIENT_PHONE_VIEW_NUMBERS.phone_number FROM patientDB.patient_info_view as PATIENT_INFO_VIEW INNER JOIN patientDB.patients_phone_number_view AS PATIENT_PHONE_VIEW_NUMBERS ON PATIENT_INFO_VIEW.patient_id = PATIENT_PHONE_VIEW_NUMBERS.patient_id",
+          "CREATE VIEW `patient_view` AS SELECT PATIENT_INFO_VIEW.patient_id AS id, PATIENT_INFO_VIEW.clinic_id, PATIENT_INFO_VIEW.primary_health_plan_number, CONCAT(PATIENT_INFO_VIEW.first_name, ' ', PATIENT_INFO_VIEW.last_name) AS full_name, PATIENT_INFO_VIEW.dob, PATIENT_INFO_VIEW.created_at, PATIENT_INFO_VIEW.created_by, PATIENT_INFO_VIEW.updated_at, PATIENT_INFO_VIEW.updated_by, PATIENT_INFO_VIEW.deleted_at, PATIENT_INFO_VIEW.deleted_by, PATIENT_PHONE_VIEW_NUMBERS.primary, PATIENT_PHONE_VIEW_NUMBERS.phone_type_code, PATIENT_PHONE_VIEW_NUMBERS.phone_number FROM PatientManagement.patient_info_view as PATIENT_INFO_VIEW INNER JOIN PatientManagement.patients_phone_number_view AS PATIENT_PHONE_VIEW_NUMBERS ON PATIENT_INFO_VIEW.patient_id = PATIENT_PHONE_VIEW_NUMBERS.patient_id",
         );
       })
       .then(() => {
         queryInterface.sequelize.query(
-          'CREATE VIEW `patients_view` AS SELECT * FROM patientDB.patient_view',
+          'CREATE VIEW `patients_view` AS SELECT * FROM PatientManagement.patient_view',
         );
       });
   },
