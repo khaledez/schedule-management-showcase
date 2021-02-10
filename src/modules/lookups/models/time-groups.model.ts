2f2f05afbac4cecf04b820a9b72f50844fa89be8
mahmoud- <mahmoudihmaid@gmail.com>
2021-02-10 10:56:16 +0200

    Mahmoud [ADD] exclude deletedAt, deletedBy from appsync

--- src/modules/lookups/models/time-groups.model.ts
+++ src/modules/lookups/models/time-groups.model.ts
@@ -1,11 +1,6 @@
 import { Table, Column, DefaultScope } from 'sequelize-typescript';
 import { LookupsModel } from '../../../common/models/lookup-model';
 
-@DefaultScope(() => ({
-  attributes: {
-    include: ['id', 'clinicId', 'nameEn', 'nameFr', 'startTime', 'endTime'],
-  },
-}))
 @Table({ tableName: 'TimeGroupsLookups', underscored: true })
 export class TimeGroupsLookupsModel extends LookupsModel {
   @Column
