dc7f1a5aefdae1c15e96ff938fcc29ca1b8a0f00
mahmoud- <mahmoudihmaid@gmail.com>
2021-02-10 14:51:21 +0200

    Mahmoud [FIX] get rid phone_numbers from the view

---
+++ src/db/migrations/20210210123820-update-patient-views.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.sequelize.query('DROP VIEW `patients_phone_number_view`;'),
+      queryInterface.sequelize.query('DROP VIEW `patients_view`;'),
+      queryInterface.sequelize.query(
+        "CREATE VIEW `patients_view` AS SELECT PATIENT_INFO_VIEW.patient_id AS id, PATIENT_INFO_VIEW.clinic_id, PATIENT_INFO_VIEW.primary_health_plan_number, CONCAT(PATIENT_INFO_VIEW.first_name, ' ', PATIENT_INFO_VIEW.last_name) AS full_name, PATIENT_INFO_VIEW.dob, PATIENT_INFO_VIEW.created_at, PATIENT_INFO_VIEW.created_by, PATIENT_INFO_VIEW.updated_at, PATIENT_INFO_VIEW.updated_by, PATIENT_INFO_VIEW.deleted_at, PATIENT_INFO_VIEW.deleted_by FROM patient_info_view as PATIENT_INFO_VIEW",
+      ),
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.sequelize.query('DROP VIEW `patients_view`;'),
+      queryInterface.sequelize.query(
+        'CREATE VIEW `patients_phone_number_view`;',
+      ),
+    ]);
+  },
+};
