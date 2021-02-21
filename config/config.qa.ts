84d4bd2665cb414ef5014e5ecc2569ce667f5b2d
mahmoud- <mahmoudihmaid@gmail.com>
2021-02-21 15:00:11 +0200

    Mahmoud [ADD] config file

---
+++ config/config.qa.ts
@@ -0,0 +1,7 @@
+export const config = () => ({
+    cognito: {
+        userPoolId: 'ca-central-1_P6vKnQ2LG',
+        clientId: '33qg4ep2bligir0h4l7nbdcjuj',
+        region: 'ca-central-1',
+    },
+});
