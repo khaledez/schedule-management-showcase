afd44bed1d8504f7f894822a4c55e83f60878d5b
mahmoud- <mahmoudihmaid@gmail.com>
2021-02-24 15:09:22 +0200

    Mahmoud [FIX] pagination not supported

--- src/common/interceptor/pagination.interceptor.ts
+++ src/common/interceptor/pagination.interceptor.ts
@@ -6,62 +6,130 @@ import {
   Logger,
   NestInterceptor,
   InternalServerErrorException,
+  BadRequestException,
 } from '@nestjs/common';
 import { SEQUELIZE } from '../constants';
 import { Observable } from 'rxjs';
 import { Sequelize } from 'sequelize-typescript';
 import { Reflector } from '@nestjs/core';
 import { map } from 'rxjs/operators';
-import { Op } from 'sequelize';
+import { ErrorCodes } from '../enums/error-code.enum';
+import { ConfigService } from '@nestjs/config';
+import { PaginationConfig } from '../interfaces/pagination-config.interface';
 
 @Injectable()
 export class PaginationInterceptor implements NestInterceptor {
   private readonly logger = new Logger('PaginationInterceptor');
   constructor(
-    @Inject(SEQUELIZE) private readonly sequalize: Sequelize,
+    @Inject(SEQUELIZE) private readonly sequelize: Sequelize,
     private reflector: Reflector,
+    private configService: ConfigService,
   ) {}
+  // eslint-disable-next-line complexity
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle().pipe(
-      // eslint-disable-next-line complexity
-      map((res) => {
-        const { data, limit, hasPreviousPage = false } = res;
-        // get the incoming model
-        const model = this.reflector.get<string>('modle', context.getHandler());
-        this.logger.debug(`incomming model is ${model}`);
-        if (!model) {
-          this.logger.error('modle not set in the meta data');
-          throw new InternalServerErrorException({
-            code: 'SERVER_ERROR',
-            message: 'Internal Server Error',
-          });
-        }
-        // check if the modle already exists
-        if (!this.sequalize.models[model]) {
-          this.logger.error(`modle: ${model} not set in the system`);
-          throw new InternalServerErrorException({
-            code: 'SERVER_ERROR',
-            message: 'Internal Server Error',
-          });
-        }
-        // get the page info
-        const dataLength = data.length;
-        const hasNextPage = dataLength > limit;
-        const startCursor = dataLength ? data[0].id : null;
-        const endCursor = dataLength ? data[dataLength - 1].id : null;
+    try {
+      const request = context.switchToHttp().getRequest();
+      const { first, last, before, after } = request.body;
+      if (
+        (!!first && !!last) ||
+        (!!before && !!after) ||
+        first < 0 ||
+        last < 0
+      ) {
+        throw new BadRequestException({
+          code: ErrorCodes.INVALID_INPUT,
+          message: 'Invalid Query filters!',
+        });
+      }
+      if ((!!first && !!before) || (!!last && !!after)) {
+        throw new BadRequestException({
+          code: ErrorCodes.INVALID_INPUT,
+          message: 'UnSupported pagination way!',
+        });
+      }
+      const {
+        max,
+        default: defaultLimit,
+      } = this.configService.get<PaginationConfig>('paginationInfo');
+      const limit = (first || last || defaultLimit) as number;
 
-        return {
-          edges: data.map((node: any) => ({
+      const finalLimit = max <= limit ? max : limit;
+      let offset: number;
+      if (first && after) {
+        offset = after;
+      } else {
+        offset = 0;
+      }
+      request.pagingInfo = {
+        limit: finalLimit,
+        offset: offset < 0 ? 0 : offset,
+      };
+      this.logger.debug(`request.pagingInfo ${request.pagingInfo}`);
+
+      return next.handle().pipe(
+        // eslint-disable-next-line complexity
+        map((res) => {
+          const { data, hasPreviousPage = false, count: total } = res;
+          // get the incoming model
+          const model = this.reflector.get<string>(
+            'model',
+            context.getHandler(),
+          );
+          this.logger.debug(`incomming model is ${model}`);
+          if (!model) {
+            this.logger.error('model not set in the meta data');
+            throw new InternalServerErrorException({
+              code: 'SERVER_ERROR',
+              message: 'Internal Server Error',
+            });
+          }
+          const dbModel = this.sequelize.models[model];
+          // check if the model already exists
+          if (!dbModel) {
+            this.logger.error(`model: ${model} not set in the system`);
+            throw new InternalServerErrorException({
+              code: 'SERVER_ERROR',
+              message: 'Internal Server Error',
+            });
+          }
+          const modifiedDataAsEdges = data.map((node: any, index) => ({
+            // start cursor from 1
+            cursor: (!last ? offset + index : total + offset - index) + 1,
             node,
-          })),
-          pageInfo: {
-            hasNextPage,
-            hasPreviousPage,
-            startCursor,
-            endCursor,
-          },
-        };
-      }),
-    );
+          }));
+          this.logger.debug({
+            function: 'PaginationInterceptor modifiedDataAsEdges',
+            modifiedDataAsEdges,
+          });
+          // get the page info
+          const hasNextPage = total > finalLimit + offset;
+          const startCursor = modifiedDataAsEdges.length
+            ? modifiedDataAsEdges[0].cursor
+            : null;
+          const endCursor = modifiedDataAsEdges.length
+            ? modifiedDataAsEdges[modifiedDataAsEdges.length - 1].cursor
+            : null;
+
+          return {
+            edges: modifiedDataAsEdges,
+            pageInfo: {
+              hasNextPage: !last ? hasNextPage : false,
+              hasPreviousPage: !first && last ? hasPreviousPage : false,
+              startCursor,
+              endCursor,
+            },
+          };
+        }),
+      );
+    } catch (error) {
+      this.logger.error({
+        function: 'PaginationInterceptor',
+        error,
+      });
+      throw new InternalServerErrorException({
+        code: 'SERVER_ERROR',
+        message: 'Internal Server Error',
+      });
+    }
   }
 }
--- src/modules/appointments/appointments.controller.ts
+++ src/modules/appointments/appointments.controller.ts
@@ -10,6 +10,7 @@ import {
   UseInterceptors,
   SetMetadata,
   Patch,
+  Req,
 } from '@nestjs/common';
 import { AppointmentsService } from './appointments.service';
 import { CreateAppointmentProvisionalBodyDto } from './dto/create-appointment-provisional-body.dto';
@@ -27,6 +28,8 @@ import { FilterBodyDto } from 'src/common/dtos/filter-body.dto';
 import { PaginationInterceptor } from '../../common/interceptor/pagination.interceptor';
 import { LookupsService } from '../lookups/lookups.service';
 import { AppointmentStatusEnum } from 'src/common/enums/appointment-status.enum';
+import { PagingInfo } from '../../common/decorators/pagingInfo.decorator';
+import { PagingInfoInterface } from 'src/common/interfaces/pagingInfo.interface';
 
 @Controller('appointments')
 export class AppointmentsController {
@@ -38,49 +41,26 @@ export class AppointmentsController {
 
   // search using post method
   @UseInterceptors(PaginationInterceptor)
-  @SetMetadata('modle', 'AppointmentsModel')
+  @SetMetadata('model', 'AppointmentsModel')
   @Post('search')
-  search(@Identity() identity: IdentityDto, @Body() body: FilterBodyDto) {
+  search(
+    @Identity() identity: IdentityDto,
+    @PagingInfo() pagingInfo: PagingInfoInterface,
+    @Body() body: FilterBodyDto,
+  ) {
     this.logger.debug({
       function: 'controller/appointment/search',
       identity,
       body,
     });
-    const { first, last, before, after } = body;
-    if ((!!first && !!last) || (!!before && !!after)) {
-      throw new BadRequestException({
-        code: ErrorCodes.INVALID_INPUT,
-        message: 'Invalid Query filters!',
-      });
-    }
-    return this.appointmentsService.findAll({ query: body, identity });
-  }
-  /**
-   * Find all appointments
-   * @param identity
-   */
-  // TODO: Remove this function
-  @Get()
-  findAll(
-    @Identity() identity: IdentityDto,
-    @Query() query: QueryParamsDto,
-  ): Promise<AppointmentResponseInterface[]> {
-    this.logger.debug({
-      function: 'controller/appointment/findAll',
+    return this.appointmentsService.findAll({
+      query: body,
       identity,
-      query,
+      pagingInfo,
     });
-    const { first, last, before, after } = query;
-    if ((!!first && !!last) || (!!before && !!after)) {
-      throw new BadRequestException({
-        code: ErrorCodes.INVALID_INPUT,
-        message: 'Invalid Query filters!',
-      });
-    }
-    return this.appointmentsService.findAll({ query, identity });
   }
 
-  // get total appointment for each day for aspecific period
+  // get total appointment for each day for a specific period
   @Get('appointments-days')
   async getAppointmentsByPeriods(
     @Identity() identity: IdentityDto,
