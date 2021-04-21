7ec2be7ee5419edf7e9bca6320480844b69d61aa
nidal <nidal.banna@gmail.com>
2021-04-21 13:11:02 +0300

    Update database.providers.ts

--- src/modules/database/database.providers.ts
+++ src/modules/database/database.providers.ts
@@ -28,7 +28,7 @@ export const databaseProviders = [
       }
       const sequelize = new Sequelize({
         ...config,
-        dialectModule: AWSXRay.captureMySQL(require('mysql2')),
+        dialectModule: AWSXRay.captureMySQL(require('mysql')),
       });
       sequelize.addModels([
         AppointmentsModel,
