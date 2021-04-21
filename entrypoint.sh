552dc1a7fd286cda00a768d2fe90dc4418d2f891
nidal <nidal.banna@gmail.com>
2021-04-21 15:33:11 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,10 @@
 npm config set color false
 
 echo "Starting Docker container in $NODE_ENV ..."
-npx sequelize db:create
+#npx sequelize db:create
 
 echo "Running Database Migrations ... "
-npx sequelize db:migrate
+#npx sequelize db:migrate
+npm run db:migrate
 
 "$@"
