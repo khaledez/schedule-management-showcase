481377e0102276b74346babc3894bb37be140360
mbanna <m.albanna@gmail.com>
2021-05-06 22:28:09 +0300

    add adhoc appointment route

---
+++ src/modules/appointments/dto/create-appointment-adhoc.dto.ts
@@ -0,0 +1,17 @@
+import { IsNumber, IsDate, IsString } from 'class-validator';
+import { Transform, Type } from 'class-transformer';
+import { IsPastDate } from 'src/utils/IsPastDate';
+// this dto for the body comes from the request
+export class CreateAppointmentAdhocDto {
+  @Transform((value) => Number(value))
+  @IsNumber()
+  patientId: number;
+
+  @IsString()
+  modeCode: string;
+
+  @IsDate()
+  @Type(() => Date)
+  @IsPastDate()
+  date: Date;
+}
