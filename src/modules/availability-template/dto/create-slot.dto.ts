8fa2761be2d721960f74ae130c4421ef8b4a22a4
Khaled Ezzughayyar <ezzughayyar@gmail.com>
2021-08-03 17:06:23 +0300

    add type transformation for availability template create dto

--- src/modules/availability-template/dto/create-slot.dto.ts
+++ src/modules/availability-template/dto/create-slot.dto.ts
@@ -1,3 +1,4 @@
+import { Type } from 'class-transformer';
 import { IsMilitaryTime, IsNumber } from 'class-validator';
 import { AvailabilitySlotAttributes } from '../interfaces/availability-template-slot.interface';
 
@@ -5,11 +6,13 @@ export type CreateSlotAttributes = Omit<AvailabilitySlotAttributes, 'id' | 'clin
 
 export class CreateSlotDto implements CreateSlotAttributes {
   @IsNumber()
+  @Type(() => Number)
   appointmentTypeId: number;
 
   @IsMilitaryTime({ message: 'Must be a valid representation of time in format HH:MM or HH:MM:SS' })
   startTime: string;
 
   @IsNumber()
+  @Type(() => Number)
   durationMinutes: number;
 }
