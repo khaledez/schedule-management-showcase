30c926980376faf9a3aeb047d1542d95b7b861c7
Amjad W <amjadshomali60@gmail.com>
2021-08-11 11:23:08 +0000

    update appointment status lookup table to include new codes

--- src/common/enums/appointment-status.enum.ts
+++ src/common/enums/appointment-status.enum.ts
@@ -1,7 +1,8 @@
 export enum AppointmentStatusEnum {
   WAIT_LIST = 'WAIT_LIST',
   SCHEDULE = 'SCHEDULE',
-  CONFIRM = 'CONFIRM',
+  CONFIRM1 = 'CONFIRM1',
+  CONFIRM2 = 'CONFIRM2',
   CHECK_IN = 'CHECK_IN',
   READY = 'READY',
   COMPLETE = 'COMPLETE',
