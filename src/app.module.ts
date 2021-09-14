aa983b6d0b858361fca3f3df815b4b5a6fbf5270
Khaled Ezzughayyar <ezzughayyar@gmail.com>
2021-09-15 01:05:38 +0300

    add xray interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,23 @@
 import { AuthModule, requestLoggerMiddleware } from '@monmedx/monmedx-common';
 import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { TerminusModule } from '@nestjs/terminus';
 import { AvailabilityTemplateModule } from 'modules/availability-template/availability-template.module';
 import { ConfigurationModule } from 'modules/config/config.module';
 import { PatientInfoModule } from 'modules/patient-info';
+import { XRayInterceptor } from 'xray.interceptor';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FireEventService } from './fire-event.service';
 import { GeneralHealthIndicator } from './general-health.provider';
+import { AppointmentRequestsModule } from './modules/appointment-requests/appointment-requests.module';
 import { AppointmentsModule } from './modules/appointments/appointments.module';
 import { AvailabilityModule } from './modules/availability/availability.module';
 import { CalendarModule } from './modules/calendar/calendar.module';
 import { DatabaseModule } from './modules/database/database.module';
 import { EventsModule } from './modules/events/events.module';
 import { LookupsModule } from './modules/lookups/lookups.module';
-import { AppointmentRequestsModule } from './modules/appointment-requests/appointment-requests.module';
 
 @Module({
   imports: [
@@ -34,7 +36,12 @@ import { AppointmentRequestsModule } from './modules/appointment-requests/appoin
     AppointmentRequestsModule,
   ],
   controllers: [AppController],
-  providers: [AppService, GeneralHealthIndicator, FireEventService],
+  providers: [
+    AppService,
+    GeneralHealthIndicator,
+    FireEventService,
+    { provide: APP_INTERCEPTOR, useValue: new XRayInterceptor('schedule-management') },
+  ],
 })
 export class AppModule {
   // apply logger middleware in all-over the modules.
---
+++ src/xray.interceptor.ts
@@ -0,0 +1,35 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { getNamespace, middleware, setSegment } from 'aws-xray-sdk-core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class XRayInterceptor implements NestInterceptor {
+  constructor(private readonly serviceName) {
+    middleware.setDefaultName(this.serviceName);
+  }
+
+  intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+    const ctx = context.switchToHttp();
+    const req = ctx.getRequest();
+    const res = ctx.getResponse();
+
+    const segment = middleware.traceRequestResponseCycle(req, res);
+
+    const ns = getNamespace();
+    ns.bindEmitter(req);
+    ns.bindEmitter(res);
+
+    ns.run(() => {
+      setSegment(segment);
+    });
+
+    const handled = next.handle();
+    handled.subscribe({
+      error(err) {
+        segment.addError(err);
+      },
+    });
+
+    return handled;
+  }
+}
