8b90081fc5b284f9028f200c8b66d89cb6c997d3
m.albanna <eng.banna1@gmail.com>
2021-09-19 12:54:00 +0300

    user-appointments filter by id

--- src/modules/appointments/dto/appointment-filter.dto.ts
+++ src/modules/appointments/dto/appointment-filter.dto.ts
@@ -106,4 +106,9 @@ export class AppointmentFilterDto {
   @ValidateNested()
   @Type(() => FilterIdsInputDto)
   clinicId?: FilterIdsInputDto;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => FilterIdsInputDto)
+  id?: FilterIdsInputDto;
 }
