043840db0e23f3116dd0ae2754c6000fa2b7567b
Kanna'n Hassouna <kannan@dash.ps>
2021-09-25 15:03:29 +0000

    [MMX-6071] update the production baseURL

--- config/config.production.ts
+++ config/config.production.ts
@@ -1,7 +1,8 @@
 export const config = () => ({
-    cognito: {
-        userPoolId: process.env.userPoolId ||'ca-central-1_QdrFL8ZgJ',
-        clientId: process.env.clientId ||'pjlj97r3fkhoibcv2t7ln8doi',
-        region: 'ca-central-1',
-    }
+  apiURL: 'https://api.prod.monmedx.com',
+  cognito: {
+    userPoolId: process.env.userPoolId || 'ca-central-1_QdrFL8ZgJ',
+    clientId: process.env.clientId || 'pjlj97r3fkhoibcv2t7ln8doi',
+    region: 'ca-central-1',
+  },
 });
