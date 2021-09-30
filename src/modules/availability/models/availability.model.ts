db087984e2a040654de466e957b8c8bef6d40da9
ehabarman <ehabarman@cloud.upwork.com>
2021-09-30 16:17:06 +0300

    [MMX-2232]: Enable availabilities hooks

--- src/modules/availability/models/availability.model.ts
+++ src/modules/availability/models/availability.model.ts
@@ -24,6 +24,7 @@ import { AvailabilityEventName, AvailabilityEventPublisher } from '../availabili
   attributes: {
     exclude: ['deletedAt', 'deletedBy'],
   },
+  individualHooks: true,
 }))
 @Scopes(() => ({
   full: {
