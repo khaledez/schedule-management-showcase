7c35a1c4cc392ade005a370eda06f7930988dacb
Kanna'n Hassouna <kannan@dash.ps>
2021-10-06 17:22:55 +0300

    introduce clinic settings permission

--- src/modules/clinic-settings/clinic-settings.controller.ts
+++ src/modules/clinic-settings/clinic-settings.controller.ts
@@ -1,4 +1,4 @@
-import { IUserPrincipal, UserPrincipal } from '@monmedx/monmedx-common';
+import { IUserPrincipal, PermissionCode, Permissions, UserPrincipal } from '@monmedx/monmedx-common';
 import { Controller, Get } from '@nestjs/common';
 import { ClinicSettingsService } from './clinic-settings.service';
 
@@ -7,6 +7,7 @@ export class ClinicSettingController {
   constructor(private readonly clinicSettingsService: ClinicSettingsService) {}
 
   @Get()
+  @Permissions(PermissionCode.CLINIC_SETTINGS_READ)
   clinicSetting(@UserPrincipal() principal: IUserPrincipal) {
     return this.clinicSettingsService.getClinicSettings(principal);
   }
